fbab8c4c6fc579fa5767b9bc1ea61fef5e0c0c59
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-09-22 22:05:05 -0500

    test: prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginTenantComponent } from './public/pages/login-tenant/login-tenant.component';
+import { LoginOwnerComponent } from './public/pages/login-owner/login-owner.component';
+import { RegisterTenantComponent } from './public/pages/register-tenant/register-tenant.component';
+import { RegisterOwnerComponent } from './public/pages/register-owner/register-owner.component';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
+import { RouterOutlet } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { NgOptimizedImage } from "@angular/common";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { FormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginTenantComponent,
+    LoginOwnerComponent,
+    RegisterTenantComponent,
+    RegisterOwnerComponent,
+    PageNotFoundComponent,
+
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    NgOptimizedImage,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
